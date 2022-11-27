434626dac32be7c186388afc5750637a683af6cb
tamaslud <tamaslud@yahoo.com>
2022-11-27 16:17:42 +0100

    nem jó.

---
+++ src/app/page/event-editor/event-editor.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { EventService } from 'src/app/service/event.service';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Event } from 'src/app/model/event';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-event-editor',
+  templateUrl: './event-editor.component.html',
+  styleUrls: ['./event-editor.component.scss']
+})
+export class EventEditorComponent implements OnInit {
+
+  event = new Event;
+
+  constructor(
+    private eventService: EventService,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(
+      params =>
+        this.eventService.get(params['id']).subscribe(
+          event => {
+            this.event = event || new Event();
+          }
+        )
+    );
+  }
+
+  onFormSubmit(form: NgForm): void {
+    console.log(form.value);
+
+  }
+}
